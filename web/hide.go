7fc9e5fdeb6e38a74cdd6df6c113f3a03355000f
xmdhs <xmdhss@gmail.com>
2021-07-04 01:09:08 +0800

    fix

--- web/hide.go
+++ web/hide.go
@@ -52,7 +52,7 @@ func Hidethead(w http.ResponseWriter, req *http.Request) {
 			Name: "无权查看的帖子",
 			List: list,
 		}
-		err = t.ExecuteTemplate(w, "page", r)
+		err = t.ExecuteTemplate(w, "html", r)
 		if err != nil {
 			panic(err)
 		}
